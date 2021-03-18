94936de93ee0ae9d1e620805169c5fb81c04bc17
Michael Lai <mrlai7782@gmail.com>
2021-03-18 08:32:00 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,31 +1,44 @@
-import "./App.css";
-import React from "react";
-import Counter from "./components/Counter";
+//import "./App.css";
+import React, { useState, useEffect } from "react";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+const App = () => {
+  const [color, setColor] = useState("black");
 
-    this.state = {
-      mount: true,
+  useEffect(() => {
+    console.log("mount");
+    const changeColorOnClick = () => {
+      if (color === "black") {
+        setColor("red");
+      } else {
+        setColor("black");
+      }
     };
 
-    this.mountCounter = () => this.setState({ mount: true });
-    this.unmountCounter = () => this.setState({ mount: false });
-  }
-  render() {
-    return (
-      <div className="App">
-        <button onClick={this.mountCounter} disabled={this.state.mount}>
-          Mount Counter
-        </button>
-        <button onClick={this.unmountCounter} disabled={!this.state.mount}>
-          Unmount Counter
-        </button>
-        {this.state.mount ? <Counter /> : null}
+    document.addEventListener("click", changeColorOnClick);
+
+    return () => {
+      console.log("unmount");
+    };
+  }, [color]);
+
+  return (
+    <div>
+      <div
+        id="myDiv"
+        style={{
+          color: "white",
+          width: "100px",
+          height: "100px",
+          position: "absolute",
+          left: "50%",
+          top: "50%",
+          backgroundColor: color,
+        }}
+      >
+        This div can change color. Click on me!
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
